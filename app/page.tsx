42804b658f917802a6384ec18d35a573f77d873b
Balkishun Maurya <140256426+Bkishun@users.noreply.github.com>
2025-06-05 23:42:20 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl text-brand">DriveIt- the ultimate storage for your file</h1>
+    </div>
+  );
+}
+ 
